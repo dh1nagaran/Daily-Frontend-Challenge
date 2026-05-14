6418597fcaf25a4711b138b0bdedd1b2d42fe84f
Dhinagaran <124670153+dh1nagaran@users.noreply.github.com>
2026-05-14 21:18:40 +0530

    Update script.js
    Refactor the event listener for adding and deleting items in the todo list. Improved user feedback for empty input and existing items.

--- Todo/script.js
+++ Todo/script.js
@@ -1,28 +1,33 @@
-const trash = `<i data-lucide="trash-2"></i>`;
 let input = document.getElementById("tex");
 let c2 = document.getElementById("contain-2");
 let p = document.getElementById("para");
 let btn = document.querySelector("button");
+let data = [];
 btn.addEventListener("click", () => {
-  let data = [];
-  if (data.indexOf(input.value) != -1) p.textContent = "Data already exists";
+  if (!input.value) p.textContent = "Enter the List";
+  else if (data.indexOf(input.value) != -1)
+    p.textContent = "List Already Exists";
   else {
+    p.textContent = "";
     data.push(input.value);
-    data.forEach((d) => {
-      let label = document.createElement("label");
-      label.innerHTML += `<span class="text">${d}</span>  <i data-lucide="square-pen" class="icon"></i> <i data-lucide="trash-2"  class="icon delete-btn"></i>`;
-      c2.append(label);
-      const del = label.querySelector(".delete-btn");
-      del.addEventListener("click", () => {
-        let text = label.querySelector(".text").textContent;
-        data = data.filter((t) => {
-          return t !== text;
-        });
-        label.remove();
-      });
+    let label = "";
+    data.map((d) => {
+      label += `<div class="list-contain"><span class="text">${d}</span>  <i data-lucide="trash-2" class="icon delete-btn" ></i></div>`;
       lucide.createIcons();
     });
-    console.log(data);
+    c2.innerHTML = label;
+    lucide.createIcons();
+
+    c2.addEventListener("click", (e) => {
+      const btnn = e.target.closest(".delete-btn");
+      if (btnn) {
+        const pdiv = btnn.closest(".list-contain");
+        const textToRemove = pdiv.querySelector(".text").textContent;
+        data = data.filter((item) => item !== textToRemove);
+        pdiv.remove();
+      }
+    });
+    input.value = "";
   }
 });
 lucide.createIcons();
